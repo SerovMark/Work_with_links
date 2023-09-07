c8257682ed7875f13bf1d006a202f931691a7b25
SerovMark <serovmark2905@gmail.com>
2023-09-07 19:58:23 +0900

    Сравнили ссылки на равенство

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
-def open_file():
-    with open("file_1.txt", "r"):
-        pass
+def open_file(file_name):
+    links = []
+    with open(file_name, "r", encoding="utf-8") as f:
+        while True:
+            line = f.readline()
+            if not line:
+                break
+            links.append(line.strip())
+    return links
 
 
-def read_links_in_list():
-    pass
+def compare(file_name):
+    with open(file_name, "w", encoding="utf-8") as f:
+        for word1 in links1:
+            for word2 in links2:
+                if word1 == word2:
+                    f.write(word2)
+
+
+links1 = open_file("file_1.txt")
+links2 = open_file("file_2.txt")
+compare("Совпадающие ссылки.txt")
+print(links1)
+print(links2)
