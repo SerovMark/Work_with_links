4460c0d5089cbf5fadfa942e7e65ce2e6a5a7477
SerovMark <serovmark2905@gmail.com>
2023-09-12 19:27:26 +0900

    Почти всё

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ def open_file(file_name):
     with open(file_name, "r", encoding="utf-8") as f:
         while True:
             line = f.readline()
+            line = line.replace("!", "")
             if not line:
                 break
             links.append(line.strip())
@@ -14,11 +15,28 @@ def compare(file_name):
         for word1 in links1:
             for word2 in links2:
                 if word1 == word2:
-                    f.write(word2)
+                    f.write(word2 + "\n")
+
+
+def no_compare(file_name):
+    with open(file_name, "w", encoding="utf-8") as f:
+        for word1 in links1:
+            for word2 in links2:
+                if word1 == word2:
+                    break
+            else:
+                f.write(word1 + "\n")
+        for word1 in links2:
+            for word2 in links1:
+                if word1 == word2:
+                    break
+            else:
+                f.write(word1 + "\n")
 
 
 links1 = open_file("file_1.txt")
 links2 = open_file("file_2.txt")
 compare("Совпадающие ссылки.txt")
+no_compare("Несовпадающие ссылки.txt")
 print(links1)
 print(links2)
